e7a1f083e8e7594f79fe21c8f5134971b69269e9
AmirSoley <amirreza.soleimani80@gmail.com>
2026-09-23 12:46:14 +0330

    feat: add bloodlust kill streak reward

---
+++ Bloodlust.cpp
@@ -0,0 +1,19 @@
+//
+// Created by parsian on 9/23/2026.
+//
+
+#include <iostream>
+#include "Bloodlust.h"
+#include "Character.h"
+
+void Bloodlust::onCharacterKilled(const CharacterKilled &event) {
+    countList[event.killer] += 1;
+    if (countList.find(event.killer)->second == 3){
+        event.killer->addMana(20);
+        event.killer->heal(30);
+        std::cout << "20 mana and 30 Health reward for "
+                  << event.killer->getName()
+                  << std::endl;
+        countList[event.killer] = 0;
+    }
+}
---
+++ Bloodlust.h
@@ -0,0 +1,20 @@
+//
+// Created by parsian on 9/23/2026.
+//
+
+#ifndef UNTITLED3_BLOODLUST_H
+#define UNTITLED3_BLOODLUST_H
+
+#include <unordered_map>
+#include "CharacterKilled.h"
+
+class Bloodlust {
+private:
+    std::unordered_map<const Character*,int>countList;
+
+public:
+    void onCharacterKilled(const CharacterKilled& event);
+};
+
+
+#endif //UNTITLED3_BLOODLUST_H
